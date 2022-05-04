897fbeb49d4352ccc054b194f8d24d79ecda4c99
GiovanniCalia <johnca@outlook.it>
2022-05-04 14:54:21 +0200

    Riuscita passaggio dati con il database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class PageController extends Controller
 {
     public function index(){
+        $myMovies = Movie::where('id', 1)->get();
+        dd($myMovies);
         return view('index');
     }
 }
